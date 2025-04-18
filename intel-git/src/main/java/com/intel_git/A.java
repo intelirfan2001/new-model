eacc38969a3b1f48bf97d93d3c412e33cf3e1d35
bhargav <bhargav3@gamil.com>
2025-04-18 11:33:44 +0530

    default 2365 changed and added new class A and B also -now added main -sout adeed

--- intel-git/src/main/java/com/intel_git/A.java
+++ intel-git/src/main/java/com/intel_git/A.java
@@ -1,4 +1,7 @@
 package com.intel_git;
 
 public class A {
+    public static void main(String[] args) {
+        System.out.println("Hello from A");
+    }
 }
--- intel-git/src/main/java/com/intel_git/B.java
+++ intel-git/src/main/java/com/intel_git/B.java
@@ -1,4 +1,7 @@
 package com.intel_git;
 
 public class B {
+    public static void main(String[] args) {
+        System.out.println("hello from B");
+    }
 }
